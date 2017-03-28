34f768e8ae3237df4357885d90c884c86f0bb734
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-03-28 17:14:44 +1300

    Load data

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "Data.h"
 #include "DNest4/code/DNest4.h"
 #include "MyModel.h"
 
 int main(int argc, char** argv)
 {
+    // Load data
+    Celery::Data::get_instance().load("data.txt");
+
     DNest4::start<Celery::MyModel>(argc, argv);
     return 0;
 }
