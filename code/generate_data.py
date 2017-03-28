5675c0ebc00a2d7140d934336a9683a9fa5d7a44
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-03-28 17:14:51 +1300

    Shitty old fake data

---
+++ code/generate_data.py
@@ -0,0 +1,30 @@
+from pylab import *
+
+seed(0)
+t = sort(1000*rand(501))
+
+[t1, t2] = meshgrid(t, t)
+dt = t1 - t2
+C = exp(-abs(dt)/60)*cos(2*pi*abs(dt)/20)
+
+n = matrix(randn(len(t))).T
+L = cholesky(C)
+
+y = (L*n).T
+y += 0.1*randn(len(t))
+
+data = empty((len(t), 3))
+data[:,0], data[:,1], data[:,2] = t, y, 0.1
+savetxt('data.txt', data)
+
+plot(data[:,0], data[:,1], 'bo-')
+show()
+
+# Log likelihood
+y = matrix(data[:,1]).T
+for i in xrange(0, len(t)):
+  C[i, i] += 0.1**2
+L = cholesky(C)
+logl = -0.5*len(t)*log(2*pi) - 0.5*2*sum(log(diag(L))) - 0.5*y.T*inv(C)*y
+print(logl)
+
