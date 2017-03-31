5f33c88b68a28646d07de2c8a1ebed84e2f64ea0
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-01 12:51:52 +1300

    Can specify data file on command line

--- code/main.cpp
+++ code/main.cpp
@@ -5,10 +5,28 @@
 
 int main(int argc, char** argv)
 {
-    // Load data
-    Celery::Data::get_instance().load("data.txt");
+    // Process command line options
+    DNest4::CommandLineOptions clos(argc, argv);
 
-    DNest4::start<Celery::MyModel>(argc, argv);
+    // Get specified data file. If none, ask the user.
+    std::string data_file = clos.get_data_file();
+    if(data_file.length() == 0)
+    {
+        std::cout << "# Enter the name of the data file you want to load: ";
+        std::cin >> data_file;
+    }
+
+    // Save the data filename
+    std::fstream fout("run_data.txt", std::ios::out);
+    fout<<data_file;
+    fout.close();
+
+    // Load data
+    Celery::Data::get_instance().load(data_file.c_str());
+
+    // Run DNest4.
+    DNest4::start<Celery::MyModel>(clos);
+
     return 0;
 }
 
