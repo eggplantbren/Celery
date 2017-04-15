86cfa2329075a4efd0d2807f41d9e57cc971e0b3
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-15 20:27:01 +1200

    More great plotting

--- code/showresults.py
+++ code/showresults.py
@@ -9,21 +9,50 @@ dn4.postprocess()
 posterior_sample = dn4.my_loadtxt("posterior_sample.txt")
 indices = dn4.load_column_names("posterior_sample.txt")["indices"]
 
+
+# Extract amplitudes
+start = indices["amplitude[0]"]
+end   = indices["period[0]"]
+all_amplitudes = posterior_sample[:, start:end].flatten()
+all_amplitudes = all_amplitudes[all_amplitudes != 0.0]
+
 # Periods
 start = indices["period[0]"]
 end   = indices["quality[0]"]
 all_periods = posterior_sample[:, start:end].flatten()
 all_periods = all_periods[all_periods != 0.0]
 
+# Extract quality factors
+start = indices["quality[0]"]
+end   = indices["sigma_boost_factor"]
+all_qualities = posterior_sample[:, start:end].flatten()
+all_qualities = all_qualities[all_qualities != 0.0]
+
 # Trim periods
-all_periods.sort()
-start, end = int(0.025*len(all_periods)), int(0.975*len(all_periods))
-all_periods = all_periods[start:end]
+temp = all_periods.copy()
+temp.sort()
+use = (all_periods >= temp[int(0.01*len(temp))]) & \
+      (all_periods <= temp[int(0.99*len(temp))])
 
 # Histogram of inferred periods
-plt.hist(all_periods, 500, color=[0.2, 0.2, 0.4])
+plt.hist(all_periods[use], 500, color=[0.2, 0.2, 0.4])
 plt.xlabel("Period")
 plt.ylabel("Relative probability")
+plt.show()
+
+# Histogram of inferred periods, weighted by amplitude
+plt.hist(all_periods[use], bins=500,
+         weights=all_amplitudes[use], alpha=0.5)
+plt.xlabel("Period")
+plt.ylabel("Relative expected amplitude")
+plt.show()
+
+# Plot period vs. quality factor
+plt.plot(all_periods[use],
+         all_qualities[use],
+         ".", markersize=1, alpha=0.1)
+plt.xlabel("Period")
+plt.ylabel("Quality factor")
 plt.show()
 
 # Histogram of number of modes
