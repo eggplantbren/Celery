a6ecf7bdd3b79e08415dae03f85adfbb9c545faa
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-02-27 12:10:05 +1300

    Periodogram, and example_data sounds better

--- code/generate_data.py
+++ code/generate_data.py
@@ -1,9 +1,10 @@
 from pylab import *
+from numba import jit
 
 # This is the script that was used to generate example_data.txt
 
 seed(0)
-t = sort(500*rand(3001))
+t = sort(1500*rand(501))
 
 [t1, t2] = meshgrid(t, t)
 dt = t1 - t2
@@ -14,14 +15,38 @@ n = matrix(randn(len(t))).T
 L = cholesky(C)
 
 y = (L*n).T
-y += 0.15*randn(len(t))
+y += 0.35*randn(len(t))
 
 data = empty((len(t), 3))
-data[:,0], data[:,1], data[:,2] = t, y, 0.1
-savetxt('fake_data.txt', data)
+data[:,0], data[:,1], data[:,2] = t, y, 0.3
+savetxt('example_data.txt', data)
 
-plot(data[:,0], data[:,1], '-')
+figure(figsize=(12,6))
+errorbar(data[:,0], data[:,1], yerr=data[:,2], fmt=".", markersize=5)
+xlabel("Time")
+ylabel("Measurement")
 show()
+
+@jit
+def periodogram(periods):
+    """
+    Compute the periodogram.
+    """
+    pgram = empty(ps.size)
+    for i in range(len(periods)):
+        sine   = sin(2*pi*t/periods[i])
+        cosine = cos(2*pi*t/periods[i])
+        pgram[i] = sum(sine*y/data[:,2])**2 + sum(cosine*y/data[:,2])**2
+    return pgram
+
+ps = linspace(0, 50, 1001)
+pgram = periodogram(ps)
+plot(ps, pgram)
+xlabel("Period")
+ylabel("Power")
+title("Periodogram")
+show()
+
 
 # Log likelihood
 y = matrix(data[:,1]).T
